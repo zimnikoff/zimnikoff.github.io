65b791af20506246f0893903287dbdc2374eb825
Sasha Zimnikoff <45745083+zimnikoff@users.noreply.github.com>
2019-03-04 06:41:30 +0300

    Add files via upload

--- ThePesni/js/common.js
+++ ThePesni/js/common.js
@@ -1,33 +1,37 @@
 $(function() {
 
-	 var link = $('.menu-button');
-	 var menu = $('.m-menu');
-	 var close = $('.m-menu__close');
-	 var body = $('body');
+	var link = $('.menu-button');
+	var menu = $('.m-menu');
+	var close = $('.m-menu__close');
+	var body = $('body');
 
-	 link.on('click', function(event){
-	 	event.preventDefault();
-	 	menu.toggleClass('m-menu__active');
-	 	body.toggleClass('body-menu-active');
-	 });
+	link.on('click', function(event){
+		event.preventDefault();
+		menu.toggleClass('m-menu__active');
+		body.toggleClass('body-menu-active');
+	});
 
-	 close.on('click', function(event){
-	 	event.preventDefault();
-	 	menu.toggleClass('m-menu__active');
-	 	body.toggleClass('body-menu-active');
-	 });
+	close.on('click', function(event){
+		event.preventDefault();
+		menu.toggleClass('m-menu__active');
+		body.toggleClass('body-menu-active');
+	});
 
-	 
-	 $(window).resize(function(){
-	 	
-	 	$('.instruction__item').height('auto');
-	 	$('.principles__wrapper').height('auto');
+	
+	$(document).ready(function() {
+    $('.instruction__item').equalHeights();
+		$('.principles__wrapper').equalHeights();
+	});	 
+ 	
+	$(window).resize(function(){
+		$('.instruction__item').height('auto');
+		$('.principles__wrapper').height('auto');
 
-	 	$('.instruction__item').equalHeights();
+		$('.instruction__item').equalHeights();
 		$('.principles__wrapper').equalHeights();
 
 		$('.mainlogo_container').css({ height: window.innerHeight - 60});
-	 });
+});
 
 
 });
